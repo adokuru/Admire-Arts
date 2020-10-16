11e2d3632736a5405f980c1b267047531bd8b6dc
David Adokuru <davidadokuru@gmail.com>
2020-10-16 10:36:57 +0100

    updates

--- database/migrations/2020_10_16_092820_create_artcontacts_table.php
+++ database/migrations/2020_10_16_092820_create_artcontacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArtTable extends Migration
+class CreateArtcontactsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateArtTable extends Migration
      */
     public function up()
     {
-        Schema::create('art', function (Blueprint $table) {
+        Schema::create('artcontacts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('art_id');
+            $table->foreign('art_id')->references('id')->on('art')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateArtTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('art');
+        Schema::dropIfExists('artcontacts');
     }
 }
---
+++ database/migrations/2020_10_17_170854_create_art_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('art', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('art_category_id');
+            $table->foreign('art_category_id')->references('id')->on('art_categories')->onDelete('cascade');
+            $table->unsignedBigInteger('artist_id');
+            $table->foreign('artist_id')->references('id')->on('artists')->onDelete('cascade');
+            $table->decimal('amount', 15, 2)->nullable();
+            $table->string('description')->nullable();
+            $table->string('name');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('art');
+    }
+}
