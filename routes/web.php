a4d22f06839e6e9fe88025d72ad318f03bcdf430
David Adokuru <davidadokuru@gmail.com>
2020-10-22 11:38:52 +0100

    updates

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@Home')->name('home');
+Route::POST('/', 'App\Http\Controllers\HomeController@newsletter')->name('newsletter');
+
 
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('contact');
+Route::POST('/contact', 'App\Http\Controllers\HomeController@contactform')->name('contactform');
 
-Route::get('/our-artists', 'App\Http\Controllers\HomeController@artist');
+
 
 Route::get('/artworks', 'App\Http\Controllers\HomeController@artwork');
-
-Route::get('/view-rooms', 'App\Http\Controllers\HomeController@viewrooms');
-
-Route::get('/auctions', 'App\Http\Controllers\HomeController@auction');
-
 Route::POST('/artworks', 'App\Http\Controllers\HomeController@artcontact')->name('Artcontact');
 
-Route::POST('/artworks', 'App\Http\Controllers\HomeController@contactform')->name('contactform');
 
-Route::POST('/contact', 'App\Http\Controllers\HomeController@newsletter')->name('newsletter');
-
+Route::get('/view-rooms', 'App\Http\Controllers\HomeController@viewrooms');
+Route::get('/auctions', 'App\Http\Controllers\HomeController@auction');
+Route::get('/our-artists', 'App\Http\Controllers\HomeController@artist');
 Route::get('/Editorial', 'App\Http\Controllers\HomeController@blog');
-
 Route::get('/artworks/{Slug}', 'App\Http\Controllers\HomeController@artslug')->name('art.slug');
 
 
